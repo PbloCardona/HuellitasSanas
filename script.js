3923db062c8fa2830892b1b5b1f5b8456fbbb6dd
PbloCardona <p.blocardona@gmail.com>
2023-08-15 09:25:24 -0500

    Proyecto PaginaVeterinaria-V1

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("navbar-scroll-hide");
+    } else {
+        navbar.classList.remove("navbar-scroll-hide");
+    }
+
+    
+});
+
+
+
+
+
