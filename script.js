7b8b1bef77852d80faa9d8e1bbe09db51769346d
PbloCardona <128027722+PbloCardona@users.noreply.github.com>
2023-08-23 04:20:16 +0100

    animaciones y demas vrgas

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("navbar-scroll-hide");
-    } else {
-        navbar.classList.remove("navbar-scroll-hide");
-    }
-
-    
-});
 AOS.init();
 
 
