cf6645ad6155c1bef7e1e320144da87c5ec30467
SebastinBeltran <101418287+SebastinBeltran@users.noreply.github.com>
2023-09-04 16:05:56 -0500

    Actualización Servicios

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-AOS.init();
+AOS.init(1);
 
+
+//nnavbar hidder
+let prevScrollPos = window.pageYOffset;
+const navbar = document.getElementById("navbar");
+
+window.onscroll = function () {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+        navbar.classList.remove("hidden");
+    } else {
+        navbar.classList.add("hidden");
+    }
+
+    prevScrollPos = currentScrollPos;
+};
 
 
 
