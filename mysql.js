71b93790efcd39ae33df278496094d6ba5ee7482
SebastinBeltran <101418287+SebastinBeltran@users.noreply.github.com>
2023-11-08 10:24:04 -0500

    Add files via upload

--- mysql.js
+++ mysql.js
@@ -11,7 +11,25 @@ conexion.connect((err)=>{
   console.log("Conexion establecida")
 })
 
-const insert = 'INSERT INTO users (id,nombre,identificacion,telefono,correo,servicio,fecha) VALUES ("20","juan","1231321","asdasd","asdasda","asdasda","1231321")'
+// let datos = {
+//           id: idInput.value,
+//           nombre: nameInput.value,
+//           telefono: telInput.value,
+//           correo: emailInput.value,
+//           cita: servicioSelect.value,
+//           fecha: fechaInput.value
+//         }
+
+let datos = {
+    id: 121715455,
+    nombre: "Sebas",
+    telefono: "310",
+    correo: "@gmail.com",
+    cita: "medico",
+    fecha: "2023-06-12"
+  }
+
+const insert = `INSERT INTO users (id,nombre,identificacion,telefono,correo,servicio,fecha) VALUES (NULL,"${datos.nombre}",${datos.id},"${datos.telefono}","${datos.correo}","${datos.cita}","${datos.fecha}")`
   conexion.query(insert,(err,rows)=>{
     if(err) throw err 
 })
